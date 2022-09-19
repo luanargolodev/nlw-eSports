618b6415946b814313981074bf9fb8245288b85d
Luan Lemos <luan.lemos@agenciaeplus.com.br>
2022-09-18 22:12:46 -0300

    Hospedar projeto no heroku

--- mobile/src/components/GameCard/index.tsx
+++ mobile/src/components/GameCard/index.tsx
@@ -3,18 +3,20 @@ import {
   TouchableOpacityProps,
   ImageBackground,
   ImageSourcePropType,
-  Text
-} from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
+  Text,
+} from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
-import { styles } from './styles';
-import { THEME } from '../../theme';
+import { styles } from "./styles";
+import { THEME } from "../../theme";
 
 export interface GameCardProps {
   id: string;
-  name: string;
-  ads: string;
-  cover: ImageSourcePropType;
+  title: string;
+  _count: {
+    ads: number;
+  };
+  bannerUrl: string;
 }
 
 interface Props extends TouchableOpacityProps {
@@ -24,21 +26,11 @@ interface Props extends TouchableOpacityProps {
 export function GameCard({ data, ...rest }: Props) {
   return (
     <TouchableOpacity style={styles.container} {...rest}>
-      <ImageBackground
-        style={styles.cover}
-        source={data.cover}
-      >
-        <LinearGradient
-          colors={THEME.COLORS.FOOTER}
-          style={styles.footer}
-        >
-          <Text style={styles.name}>
-            {data.name}
-          </Text>
+      <ImageBackground style={styles.cover} source={{ uri: data.bannerUrl }}>
+        <LinearGradient colors={THEME.COLORS.FOOTER} style={styles.footer}>
+          <Text style={styles.name}>{data.title}</Text>
 
-          <Text style={styles.ads}>
-            {data.ads} anúncios
-          </Text>
+          <Text style={styles.ads}>{data._count.ads} anúncios</Text>
         </LinearGradient>
       </ImageBackground>
     </TouchableOpacity>
--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,38 +1,39 @@
-import { View, Image, FlatList } from 'react-native';
+import { useEffect, useState } from "react";
+import { View, Image, FlatList } from "react-native";
 
-import logoImg from '../../assets/logo-nlw-esports.png'
-import { GameCard } from '../../components/GameCard';
-import { Heading } from '../../components/Heading';
+import logoImg from "../../assets/logo-nlw-esports.png";
+import { GameCard, GameCardProps } from "../../components/GameCard";
+import { Heading } from "../../components/Heading";
 
-import { GAMES } from '../../utils/games';
-import { styles } from './styles';
+import { styles } from "./styles";
 
 export function Home() {
+  const [games, setGames] = useState<GameCardProps[]>([]);
+
+  useEffect(() => {
+    fetch("https://luanargolodev-nlw-esports.herokuapp.com/games")
+      .then((response) => response.json())
+      .then((data) => setGames(data))
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Image
-        source={logoImg}
-        style={styles.logo}
-      />
+      <Image source={logoImg} style={styles.logo} />
 
-      <Heading 
+      <Heading
         title="Encontre seu duo!"
         subtitle="Selecione o jogo que deseja jogar..."
       />
 
       <FlatList
-        data={GAMES}
-        keyExtractor={item => item.id}
-        renderItem={({ item }) => (
-          <GameCard 
-            data={item}
-          />
-        )}
+        data={games}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <GameCard data={item} />}
         horizontal
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={styles.contentList}
       />
-
     </View>
   );
 }
